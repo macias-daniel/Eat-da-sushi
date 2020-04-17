fc3e9ab1354f2a51f1e03cc0ad32c07c1753268e
PinkFished <d.macias.apps@gmail.com>
2020-04-16 22:32:20 -0700

    Updated heroku db

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,8 +3,13 @@ const sushi = require("../model/sushi")
 
 
 router.get("/", (req, res) =>{
-    sushi.all(data =>{
-        console.log(data)
+    sushi.all(sushi =>{
+
+        //the data sushi.all is recieving is an array of objects. Handle bars wants an object so put your array into an object give your array a key and pass that object to res.render in handle bar refer to that data through the arrays key that you passed through the object
+        const templateData = { sushi: JSON.stringify(sushi)}
+
+        res.render("index", templateData)
+
     })
 })  
 
